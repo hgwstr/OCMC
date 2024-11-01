381056d5213e074ad21eac8515e8bc0c6f0e8d74
hgwstr <153836350+hgwstr@users.noreply.github.com>
2024-11-01 20:49:38 +0700

    Reworked lab4.m

--- Practice4/lab4.m
+++ Practice4/lab4.m
@@ -1,82 +1,133 @@
-% Параметры последовательностей Голда
-SIZE = 5;
-length_seq = 2^SIZE - 1;
+function main
+    sequence_length = 31;
 
-% Начальные состояния регистров для двух последовательностей Голда
-regs = {
-    [0, 1, 0, 0, 0], [0, 1, 1, 1, 1];  % x = 8, y = 15
-    [0, 1, 0, 0, 1], [0, 1, 0, 1, 0]   % x = 9, y = 10
-};
+    % Первая последовательность Голда (x = 8, y = 15)
+    x1 = [0 1 0 0 0]; % 01000
+    y1 = [0 1 1 1 1]; % 01111
+    gold_sequence1 = generateGoldSequence(x1, y1, sequence_length);
 
-% Генерация последовательностей Голда
-[gold_seq1, reg1_final] = generate_sequence(regs{1,1}, regs{1,2}, length_seq);
-[gold_seq2, reg2_final] = generate_sequence(regs{2,1}, regs{2,2}, length_seq);
+    % Вторая последовательность Голда (x = 9, y = 10)
+    x2 = [0 1 0 0 1]; % 01001
+    y2 = [0 1 0 1 0]; % 01010
+    gold_sequence2 = generateGoldSequence(x2, y2, sequence_length);
 
-fprintf('Первая последовательность Голда (x = 8, y = 15):\n');
-fprintf('Последовательность Голда: %s\n\n', num2str(gold_seq1));
-fprintf('Вторая последовательность Голда (x = 9, y = 10):\n');
-fprintf('Последовательность Голда: %s\n\n', num2str(gold_seq2));
+    % Вывод первой и второй последовательности
+    disp('Первая последовательность Голда (x=8, y=15):');
+    fprintf('%d', gold_sequence1);
+    fprintf('\n');
 
-% Вычисление корреляций
-[cor1, lag1] = xcorr(gold_seq1, 'normalized');
-[cor2, lag2] = xcorr(gold_seq2, 'normalized');
-[cross_cor, lag_cross] = xcorr(gold_seq1, gold_seq2, 'normalized');
+    disp('Вторая последовательность Голда (x=9, y=10):');
+    fprintf('%d', gold_sequence2);
+    fprintf('\n');
 
-positive_lag_idx = lag1 >= 0 & lag1 <= 31;
+    % Автокорреляция с проверкой на m-последовательность
+    disp('Автокорреляция первой последовательности с проверкой на m-последовательность:');
+    calculateAutocorrelationAndCheckMSequence(gold_sequence1, sequence_length);
 
-% Построение графиков
-figure('Name', 'Корреляции последовательностей Голда');
+    % Взаимная корреляция между первой и второй последовательностями
+    cross_correlation = correlation(gold_sequence1, gold_sequence2, sequence_length);
+    fprintf(['\nВзаимная корреляция между первой и второй последовательностями: ', num2str(cross_correlation), '/', num2str(sequence_length)]);
+    fprintf('\n');
 
-subplot(3,1,1);
-stem(lag1(positive_lag_idx), cor1(positive_lag_idx));
-title('Автокорреляция первой последовательности');
-xlabel('Лаг');
-ylabel('Автокорреляция');
-xlim([0, length_seq]);
-
-subplot(3,1,2);
-stem(lag2(positive_lag_idx), cor2(positive_lag_idx));
-title('Автокорреляция второй последовательности');
-xlabel('Лаг');
-ylabel('Автокорреляция');
-xlim([0, length_seq]);
-
-subplot(3,1,3);
-stem(lag_cross(positive_lag_idx), cross_cor(positive_lag_idx));
-title('Взаимная корреляция последовательностей');
-xlabel('Лаг');
-ylabel('Взаимная корреляция');
-xlim([0, length_seq]);
-
-% Вывод значений корреляций в командное окно
-fprintf('\nАвтокорр. для первой последовательности:\n');
-for i = find(positive_lag_idx)
-    fprintf('Задержка %d: %.3f\n', lag1(i), cor1(i));
+    % Графическая визуализация
+    visualizeAutocorrelation(gold_sequence1);
+    visualizeCrossCorrelation(gold_sequence1, gold_sequence2);
 end
-
-fprintf('\nАвтокорр. для второй последовательности:\n');
-for i = find(positive_lag_idx)
-    fprintf('Задержка %d: %.3f\n', lag2(i), cor2(i));
-end
-
-fprintf('\nВзаимная корреляция:\n');
-for i = find(positive_lag_idx)
-    fprintf('Задержка %d: %.3f\n', lag_cross(i), cross_cor(i));
-end
-
 
 % Функция для генерации последовательности Голда
-function [seq, reg_final] = generate_sequence(reg_x, reg_y, length)
-    seq = zeros(1, length);
+function gold_sequence = generateGoldSequence(x, y, length)
+    gold_sequence = zeros(1, length);
     for i = 1:length
-        seq(i) = mod(reg_x(end) + reg_y(end), 2);  % XOR последних элементов регистров x и y
-        [reg_x, reg_y] = shift_registers(reg_x, reg_y);  % Сдвиг регистров
+        new_bit_x = xor(x(5), x(3)); % XOR элементов 5 и 3
+        new_bit_y = xor(y(5), y(3)); % XOR элементов 5 и 3
+        gold_sequence(i) = xor(x(5), y(5)); % Голд последовательность как XOR выходов двух регистров
+
+        % Обновляем регистры
+        x = [new_bit_x, x(1:4)];
+        y = [new_bit_y, y(1:4)];
     end
-    reg_final = {reg_x, reg_y};
 end
 
-% Функция для сдвига регистров
-function [reg_x, reg_y] = shift_registers(reg_x, reg_y)
-    reg_x = [mod(reg_x(4) + reg_x(5), 2), reg_x(1:end-1)];
-    reg_y = [mod(reg_y(3) + reg_y(5), 2), reg_y(1:end-1)];
+% Проверка сбалансированности
+function balanced = checkBalance(sequence)
+    count_ones = sum(sequence == 1);
+    count_zeros = length(sequence) - count_ones;
+    balance = abs(count_ones - count_zeros);
+    if balance <= 1
+        balanced = true;
+    else
+        balanced = false;
+    end
+
+    if balance
+       fprintf('\nm-последовательность');
+    else
+       fprintf('\nне m-последовательность');
+    end
+end
+
+% Проверка на m-последовательность
+function is_m = isMSequence(sequence)
+    if length(sequence) ~= 31
+        is_m = false;
+    else
+        is_m = checkBalance(sequence);
+    end
+end
+
+% Функция для автокорреляции с проверкой на m-последовательность
+function calculateAutocorrelationAndCheckMSequence(sequence, sequence_length)
+    for shift = 0:sequence_length - 1
+        shifted_sequence = circshift(sequence, [0 shift]);
+        corr = calculateCorrelation(sequence, shifted_sequence);
+        is_m_sequence = isMSequence(shifted_sequence);
+
+        fprintf('%4d | %s | %4d/%d | %s\n', shift, sprintf('%d ', shifted_sequence), corr, is_m_sequence);
+    end
+end
+
+% Расчет корреляции между двумя последовательностями
+function corr = correlation(seq1, seq2, length)
+    corr = sum(seq1(1:length) .* seq2(1:length));
+end
+
+% Расчет корреляции между двумя последовательностями
+function corr = calculateCorrelation(seq1, seq2)
+    corr = sum(seq1 == seq2) - sum(seq1 ~= seq2);
+end
+
+% Визуализация автокорреляции
+function visualizeAutocorrelation(sequence)
+    sequence_length = length(sequence);
+    autocorr_values = zeros(1, sequence_length);
+
+    for shift = 0:sequence_length
+        shifted_sequence = circshift(sequence, [0 shift]);
+        autocorr_values(shift + 1) = calculateCorrelation(sequence, shifted_sequence);
+    end
+
+    figure;
+    stem(0:sequence_length, autocorr_values, 'filled', 'red');
+    title('Autocorrelation of Gold Sequence');
+    xlabel('Shift');
+    ylabel('Correlation');
+    grid on;
+end
+
+% Визуализация взаимной корреляции
+function visualizeCrossCorrelation(seq1, seq2)
+    sequence_length = length(seq1);
+    crosscorr_values = zeros(1, sequence_length);
+
+    for shift = 0:sequence_length
+        shifted_seq2 = circshift(seq2, [0 shift]);
+        crosscorr_values(shift + 1) = correlation(seq1, shifted_seq2, sequence_length);
+    end
+
+    figure;
+    stem(0:sequence_length, crosscorr_values, 'filled', 'green');
+    title('Cross-Correlation of Two Gold Sequences');
+    xlabel('Shift');
+    ylabel('Correlation');
+    grid on;
 end
